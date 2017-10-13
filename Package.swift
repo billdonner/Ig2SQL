30be463cf4bc30cd78d5884f8f25f150da2f845c
Bill Donner <billdonner@gmail.com>
2017-10-13 08:35:40 -0400

    restored local mysqldriver

--- Package.swift
+++ Package.swift
@@ -4,35 +4,34 @@
 /*
  unfortunately I can not get MySQLDriver to link when referenced as a remote package
  instead I have copied in the sources and made a few fixes to accommodate swift 4
-  dependencies: [
+ dependencies: [
  .package(
  url:  "https://github.com/mcorega/MySqlSwiftNative",
  from: "1.0.0"
  )
  .target(name: "Ig2SQLCore",
  dependencies: ["Kitura","MySQLDriver"])
-import PackageDescription
  */
+
+import PackageDescription
 
 let package = Package(
     name: "Ig2SQL",
     dependencies: [
-        .package(
-            url:  "https://github.com/IBM-Swift/Kitura.git",
-            from: "1.0.0"
-        )
+      .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "1.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Health.git",  from: "0.0.0"),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.0.0")
     ],
     targets: [
         .testTarget(
             name: "Ig2SQLTests",
             dependencies: ["Ig2SQLCore"]
-            ),
-        
+        ),
         .target(
             name: "Ig2SQL",
             dependencies: ["Ig2SQLCore"]
         ),
         .target(name: "Ig2SQLCore",
-                dependencies: ["Kitura"])
+                dependencies: ["Kitura","Health"])//,"FLogger"])
     ]
 )
--- Sources/Ig2SQLCore/igtosql/bootkitura.swift
+++ Sources/Ig2SQLCore/igtosql/bootkitura.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import Kitura
+import Health
+import LoggerAPI
 
 struct ResponseModel:Codable {
     let userid:String
@@ -30,12 +32,32 @@ func generateResponse (id:String,kind:ReportKind,qparams:[String:String], finall
         finally(wr) // call back with response
     })
 }
-
+var   health = Health()
 func bootKitura() {
     // Create a new router
     let router = Router()
+   
     
     // Handle HTTP GET requests to /
+    // Serve static content from "public"
+    router.all("/", middleware: StaticFileServer())
+    
+    
+    // Basic POST request
+    router.post("/hello", handler: postHello)
+    
+    // JSON Get request
+    router.get("/json", handler: getJSON)
+    
+    // Basic application health check
+    router.get("/health", handler: getHealthCheck)
+    
+    // Basic application health check
+    router.get("/loopback", handler: getHealthCheck)
+    
+    // stawsh token stuff (temp
+    router.post("/stash", handler: postHello)
+    
 
     router.get("/report/:id/:name/") {
         request, response, next in
@@ -67,3 +89,55 @@ func bootKitura() {
     // Start the Kitura runloop (this call never returns)
     Kitura.run()
 }
+
+/**
+ * Handler for getting a text/plain response.
+ */
+public func getHello(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+    Log.debug("GET - /hello route handler...")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
+    try response.status(.OK).send("Hello from Kitura-Starter!").end()
+}
+
+/**
+ * Handler for posting the name of the entity to say hello to (a text/plain response).
+ */
+public func postHello(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+    Log.debug("POST - /hello route handler...")
+    response.headers["Content-Type"] = "text/plain; charset=utf-8"
+    if let name = try request.readString() {
+        try response.status(.OK).send("Hello \(name), from Kitura-Starter!").end()
+    } else {
+        try response.status(.OK).send("Kitura-Starter received a POST request!").end()
+    }
+}
+
+/**
+ * Handler for getting an application/json response.
+ */
+public func getJSON(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+    Log.debug("GET - /json route handler...")
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    var jsonResponse :[String:String] = [:]
+    jsonResponse["framework"]  = "Kitura"
+    jsonResponse["applicationName"]  = "IG2SQL"
+    jsonResponse["company"]  = "PurplePeople"
+    jsonResponse["organization"]  = "DonnerParties"
+    jsonResponse["location"] = "New York, NY"
+    let jsondata = try  Config.jsonEncoder.encode(jsonResponse)
+    try response.status(.OK).send(data: jsondata).end()
+}
+
+/**
+ * Handler for getting a text/plain response of application health status.
+ */
+public func getHealthCheck(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+    Log.debug("GET - /health route handler...")
+    let result = health.status.toSimpleDictionary()
+    if health.status.state == .UP {
+        try response.send(json: result).end()
+    } else {
+        try response.status(.serviceUnavailable).send(json: result).end()
+    }
+}
+
